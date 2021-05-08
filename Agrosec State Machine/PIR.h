eb02d3077b87c5dcc395176c866f18304e82a931
Olaoluwa Raji <46250887+MUDAL@users.noreply.github.com>
2021-05-08 11:32:13 +0100

    Delete Agrosec State Machine directory

--- Agrosec State Machine/PIR.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PIR_H
-#define _PIR_H
-
-extern void PIR_Init(void);
-extern bool PIR_Motion_Detected(void);
-extern void PIR_Restart(void);
-
-#endif //_PIR_H
--- Agrosec State Machine/hc06.h
+++
@@ -1,9 +0,0 @@
-#ifndef _HC06_H
-#define _HC06_H
-
-extern void HC06_Tx_Init(void);
-extern void HC06_Rx_Init(void);
-extern void HC06_Transmit(char* pData);
-extern char HC06_Receive_Char(void);
-
-#endif //_HC06_H
